b7c6d969c44cdd5e8685e514cd975029d1061896
Mohammed Tarkhany <0993790@hr.nl>
2021-04-01 14:00:00 +0200

    ew

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace CinemaApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dr scrum!");
         }
     }
 }
