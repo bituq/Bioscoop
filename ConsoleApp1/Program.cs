553672f4a523df3da9244fa9fb815ea76e023913
Mohammed Tarkhany <0993790@hr.nl>
2021-04-01 14:00:46 +0200

    waros

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace CinemaApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dr scrum!");
+            Console.WriteLine("Waros!");
         }
     }
 }
