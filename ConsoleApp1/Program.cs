a774825d813d2d5476a929fec5358ce25729bd74
Mohammed Tarkhany <0993790@hr.nl>
2021-04-02 15:14:12 +0200

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.Json;
+using System.IO;
 
 namespace CinemaApplication
 {
@@ -6,7 +8,14 @@ namespace CinemaApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dr scrum!");
+            string data = " [ {\"name\": \"John Doe\", \"occupation\": \"gardener\"}, " +
+            "{\"name\": \"Peter Novak\", \"occupation\": \"driver\"} ]";
+
+            Console.WriteLine("Enter name of movie: ");
+            var MovieName = Console.ReadLine();
+            Console.WriteLine("Enter genre: ");
+            var Genre = Console.ReadLine();
+            Console.WriteLine("...\n" + MovieName + " is saved");
         }
     }
 }
