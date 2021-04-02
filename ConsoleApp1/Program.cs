47a0c0fa471520e302f0eda13ddb382a5d2abcd2
bituq <d.noorland9@hotmail.com>
2021-04-02 13:50:32 +0200

    Made more compact

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,51 +16,14 @@ namespace CinemaApplication
             using JsonDocument doc = JsonDocument.Parse(movies);
             JsonElement root = doc.RootElement;
 
-
-
-            var u1 = root[0];
-            var u2 = root[1];
-            var u3 = root[2];
-            var u4 = root[3];
-            var u5 = root[4];
-
-
-            Console.WriteLine(u1.GetProperty("name"));
-            Console.WriteLine(u1.GetProperty("duration"));
-            Console.WriteLine(u1.GetProperty("3D"));
-            Console.WriteLine(u1.GetProperty("genre"));
-            Console.WriteLine(u1.GetProperty("rating"));
-            Console.WriteLine("");
-
-            Console.WriteLine(u2.GetProperty("name"));
-            Console.WriteLine(u2.GetProperty("duration"));
-            Console.WriteLine(u2.GetProperty("3D"));
-            Console.WriteLine(u2.GetProperty("genre"));
-            Console.WriteLine(u2.GetProperty("rating"));
-            Console.WriteLine("");
-
-            Console.WriteLine(u3.GetProperty("name"));
-            Console.WriteLine(u3.GetProperty("duration"));
-            Console.WriteLine(u3.GetProperty("3D"));
-            Console.WriteLine(u3.GetProperty("genre"));
-            Console.WriteLine(u3.GetProperty("rating"));
-            Console.WriteLine("");
-
-            Console.WriteLine(u4.GetProperty("name"));
-            Console.WriteLine(u4.GetProperty("duration"));
-            Console.WriteLine(u4.GetProperty("3D"));
-            Console.WriteLine(u4.GetProperty("genre"));
-            Console.WriteLine(u4.GetProperty("rating"));
-            Console.WriteLine("");
-
-            Console.WriteLine(u5.GetProperty("name"));
-            Console.WriteLine(u5.GetProperty("duration"));
-            Console.WriteLine(u5.GetProperty("3D"));
-            Console.WriteLine(u5.GetProperty("genre"));
-            Console.WriteLine(u5.GetProperty("rating"));
-            Console.WriteLine("");
-
-
+            foreach (JsonElement movie in doc.RootElement.EnumerateArray())
+            {
+                Console.WriteLine(movie.GetProperty("name"));
+                Console.WriteLine(movie.GetProperty("duration"));
+                Console.WriteLine(movie.GetProperty("genre"));
+                Console.WriteLine(movie.GetProperty("rating"));
+                Console.WriteLine("");
+            }
         }
     }
 }
