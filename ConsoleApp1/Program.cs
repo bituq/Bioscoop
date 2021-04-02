fa961c8d09c8602f1511c832c30c3e49d4cdd762
Brentdbr <brentdbr@hotmail.nl>
2021-04-02 15:02:09 +0200

    JSON fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,14 +15,11 @@ namespace CinemaApplication
             JsonElement root = doc.RootElement;
 
 
-            foreach (JsonElement reserverin in doc.RootElement.EnumerateArray())
-            {
-                foreach (JsonElement reservering in doc.RootElement.EnumerateArray()) {
-                    Console.WriteLine(reservering.GetProperty("reserveringNummer"));
-                    Console.WriteLine(reservering.GetProperty("film"));
-                    Console.WriteLine(reservering.GetProperty("voorNaam"));
-                    Console.WriteLine("");
-                }
+            foreach (JsonElement reservering in root.EnumerateArray()) {
+                Console.WriteLine(reservering.GetProperty("reserveringNummer"));
+                Console.WriteLine(reservering.GetProperty("film"));
+                Console.WriteLine(reservering.GetProperty("voorNaam"));
+                Console.WriteLine("");
             }
             // Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
             // Console.WriteLine(unixTimestamp);
