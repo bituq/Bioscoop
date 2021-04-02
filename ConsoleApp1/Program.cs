76ca37881fa60c355efd1dc6c1e77080af1e8cdd
Brentdbr <brentdbr@hotmail.nl>
2021-04-02 14:54:26 +0200

    json

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,18 +8,24 @@ namespace CinemaApplication
         static void Main(string[] args)
         {
             string filePath = "C:/Users/Brent/.vscode/Bioscoop/Bioscoop/ConsoleApp1/reserveringen.json";
-            using (StreamReader r= new StreamReader(filePath))
+            StreamReader reserveringFile = new StreamReader(filePath);
+            var reserveringen = reserveringFile.ReadToEnd();
+
+            JsonDocument doc = JsonDocument.Parse(reserveringen);
+            JsonElement root = doc.RootElement;
+
+
+            foreach (JsonElement reserverin in doc.RootElement.EnumerateArray())
             {
-                String s = "";
-                while ((s = r.ReadLine()) != null)
-                {
-                    if (s.Contains("voorNaam")) {
-                        Console.WriteLine(s);
-                    }
+                foreach (JsonElement reservering in doc.RootElement.EnumerateArray()) {
+                    Console.WriteLine(reservering.GetProperty("reserveringNummer"));
+                    Console.WriteLine(reservering.GetProperty("film"));
+                    Console.WriteLine(reservering.GetProperty("voorNaam"));
+                    Console.WriteLine("");
                 }
             }
-            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-            Console.WriteLine(unixTimestamp);
+            // Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            // Console.WriteLine(unixTimestamp);
         }
     }
 }
