99ad4b21db2b029cbc5e0f17651b50b11950daeb
WonderOfYou <75250387+WonderOfYou@users.noreply.github.com>
2021-04-02 13:39:32 +0200

    test2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,11 @@ namespace CinemaApplication
     {
         static void Main(string[] args)
         {
-            string movies = " [ {\"name\": \"Harry Potter\", \"duration\": \"90\", \"3D\": \"Yes\", \"genre\": \"Fantasy\", \"rating\": \"4.5/5\"}, " + "{\"name\": \"Lord of the rings\", \"duration\": \"170\"} ]";
+            string movies = " [ {\"name\": \"Harry Potter and the Deathly Hallows Part 1\", \"duration\": \"   Duration: 90 minutes\", \"3D\": \"   3D: No\", \"genre\": \"   Genre: Fantasy\", \"rating\": \"   Rating: 4/5\"}" +
+                ", " + "{\"name\": \"Lord of the rings\", \"duration\": \"   Duration: 178 minutes\", \"3D\": \"   3D: No\", \"genre\": \"   Genre: Fantasy\", \"rating\": \"   Rating: 4/5\"}" +
+                ", " + "{\"name\": \"Lord of the rings\", \"duration\": \"   Duration: 178 minutes\", \"3D\": \"   3D: No\", \"genre\": \"   Genre: Fantasy\", \"rating\": \"   Rating: 4/5\"}" +
+                ", " + "{\"name\": \"Lord of the rings\", \"duration\": \"   Duration: 178 minutes\", \"3D\": \"   3D: No\", \"genre\": \"   Genre: Fantasy\", \"rating\": \"   Rating: 4/5\"}" +
+                ", " + "{\"name\": \"Lord of the rings\", \"duration\": \"   Duration: 178 minutes\", \"3D\": \"   3D: No\", \"genre\": \"   Genre: Fantasy\", \"rating\": \"   Rating: 4/5\"} ]";
 
             using JsonDocument doc = JsonDocument.Parse(movies);
             JsonElement root = doc.RootElement;
@@ -16,8 +20,10 @@ namespace CinemaApplication
 
             var u1 = root[0];
             var u2 = root[1];
-            Console.WriteLine(u1);
-            Console.WriteLine(u2);
+            var u3 = root[2];
+            var u4 = root[3];
+            var u5 = root[4];
+
 
             Console.WriteLine(u1.GetProperty("name"));
             Console.WriteLine(u1.GetProperty("duration"));
@@ -28,6 +34,33 @@ namespace CinemaApplication
 
             Console.WriteLine(u2.GetProperty("name"));
             Console.WriteLine(u2.GetProperty("duration"));
+            Console.WriteLine(u2.GetProperty("3D"));
+            Console.WriteLine(u2.GetProperty("genre"));
+            Console.WriteLine(u2.GetProperty("rating"));
+            Console.WriteLine("");
+
+            Console.WriteLine(u3.GetProperty("name"));
+            Console.WriteLine(u3.GetProperty("duration"));
+            Console.WriteLine(u3.GetProperty("3D"));
+            Console.WriteLine(u3.GetProperty("genre"));
+            Console.WriteLine(u3.GetProperty("rating"));
+            Console.WriteLine("");
+
+            Console.WriteLine(u4.GetProperty("name"));
+            Console.WriteLine(u4.GetProperty("duration"));
+            Console.WriteLine(u4.GetProperty("3D"));
+            Console.WriteLine(u4.GetProperty("genre"));
+            Console.WriteLine(u4.GetProperty("rating"));
+            Console.WriteLine("");
+
+            Console.WriteLine(u5.GetProperty("name"));
+            Console.WriteLine(u5.GetProperty("duration"));
+            Console.WriteLine(u5.GetProperty("3D"));
+            Console.WriteLine(u5.GetProperty("genre"));
+            Console.WriteLine(u5.GetProperty("rating"));
+            Console.WriteLine("");
+
+
         }
     }
 }
