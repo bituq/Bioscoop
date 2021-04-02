cdc93dfbe49886482bbbea7ac1fbb497852391a5
WonderOfYou <75250387+WonderOfYou@users.noreply.github.com>
2021-04-02 13:13:37 +0200

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,27 @@ namespace CinemaApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Waros!");
+            string movies = " [ {\"name\": \"Harry Potter\", \"duration\": \"90\", \"3D\": \"Yes\", \"genre\": \"Fantasy\", \"rating\": \"4.5/5\"}, " + "{\"name\": \"Lord of the rings\", \"duration\": \"170\"} ]";
+
+            using JsonDocument doc = JsonDocument.Parse(movies);
+            JsonElement root = doc.RootElement;
+
+
+
+            var u1 = root[0];
+            var u2 = root[1];
+            Console.WriteLine(u1);
+            Console.WriteLine(u2);
+
+            Console.WriteLine(u1.GetProperty("name"));
+            Console.WriteLine(u1.GetProperty("duration"));
+            Console.WriteLine(u1.GetProperty("3D"));
+            Console.WriteLine(u1.GetProperty("genre"));
+            Console.WriteLine(u1.GetProperty("rating"));
+            Console.WriteLine("");
+
+            Console.WriteLine(u2.GetProperty("name"));
+            Console.WriteLine(u2.GetProperty("duration"));
         }
     }
 }
