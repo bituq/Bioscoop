27fa4c81ba2f5ea774d0587b2791f8d51d2ca0cb
Brentdbr <brentdbr@hotmail.nl>
2021-04-02 14:36:41 +0200

    wat dingen gedaan met datetime en streamreader

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-using System.Linq;
-
+using System.IO;
+using System.Text.Json;
 namespace CinemaApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filePath = "C:/Users/Brent/.vscode/Bioscoop/Bioscoop/ConsoleApp1/reserveringen.json";
+            using (StreamReader r= new StreamReader(filePath))
+            {
+                String s = "";
+                while ((s = r.ReadLine()) != null)
+                {
+                    if (s.Contains("voorNaam")) {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            Console.WriteLine(unixTimestamp);
         }
     }
 }
