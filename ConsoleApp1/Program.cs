6e9a54f83ecac0455e1cd31bbcbffdbcd0324b3b
Mohammed Tarkhany <0993790@hr.nl>
2021-04-03 22:12:15 +0200

    w

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,23 +17,43 @@ namespace CinemaApplication
             
             Console.WriteLine("Enter movie name: ");
             string name = Console.ReadLine();
+            if(name == "")
+            {                
+                while (name == "")
+                {
+                    Console.WriteLine("Error! No input is given");
+                    name = Console.ReadLine();
+                }
+            }
             Console.WriteLine("Enter duration: ");
             string duration = Console.ReadLine();
+            if (duration == "")
+            {
+                while (duration == "")
+                {
+                    Console.WriteLine("Error! No input is given");
+                    duration = Console.ReadLine();
+                }
+            }
             Console.WriteLine("Enter release date: ");
             string releasedate = Console.ReadLine();
             Console.WriteLine("Enter first genre: ");
             string genre1 = Console.ReadLine();
-            Console.WriteLine("Enter second genre (if no second genre, press enter): ");
+            Console.WriteLine("Enter second genre (if no second genre, leave empty): ");
             string genre2 = Console.ReadLine();
             string genre3 = "";
             if(genre2 != ""){
-                Console.WriteLine("Enter second genre (if no third genre, press enter): ");
+                Console.WriteLine("Enter third genre (if no third genre, leave empty): ");
                 genre3 = Console.ReadLine();
             }
             string[] genres = ((genre2 == "") ? new string[] { genre1 } : ((genre3 == "") ? new string[] { genre1, genre2 } : new string[] { genre1, genre2, genre3}));
-            addfunc(name, duration, releasedate, genres);
+            Console.WriteLine("Enter language: ");
+            string language = Console.ReadLine();
+            Console.WriteLine("Enter company: ");
+            string company = Console.ReadLine();
+            addfunc(name, duration, releasedate, genres, language, company);
 
-            static void addfunc(string name, string duration, string releasedate, string[] genres)
+            static void addfunc(string name, string duration, string releasedate, string[] genres, string language, string company)
             {
                 string filePath = "moviesadd.json";
                 StreamReader movieFile = new StreamReader(filePath);
@@ -43,7 +63,7 @@ namespace CinemaApplication
                 JsonElement root = doc.RootElement;
 
 
-                foreach (JsonElement reservering in root.EnumerateArray())
+                foreach (JsonElement movie in root.EnumerateArray())
                 {
                     string[] listmovies = movies.Split('}');
                     int len = listmovies.Length;
@@ -63,12 +83,12 @@ namespace CinemaApplication
                     string toevoegen = "\t{\n" + $"\t\t\"name\" : \"{name}\",\n" + $"\t\t\"duration\" : \"{duration}\",\n" + $"\t\t\"releasedate\" : \"{releasedate}\",\n"
                         + $"\t\t\"rating\" : \"{null}\",\n" + 
                         ((genres.Length == 1) ?
-                        $"\t\t\"genre\" : [\n\t\t\t\"{genres[0]}\"\n\t\t] \n" :
+                        $"\t\t\"genre\" : [\n\t\t\t\"{genres[0]}\"\n\t\t], \n" :
                         ((genres.Length == 2) ?
-                        $"\t\t\"genre\" : [\n\t\t\t\"{genres[0]}\",\n\t\t\t\"{genres[1]}\"\n\t\t] \n" :
-                        $"\t\t\"genre\" : [\n\t\t\t\"{genres[0]}\",\n\t\t\t\"{genres[1]}\",\n\t\t\t\"{genres[2]}\"\n\t\t] \n"))
-                        + "\t}";
-                    
+                        $"\t\t\"genre\" : [\n\t\t\t\"{genres[0]}\",\n\t\t\t\"{genres[1]}\"\n\t\t], \n" :
+                        $"\t\t\"genre\" : [\n\t\t\t\"{genres[0]}\",\n\t\t\t\"{genres[1]}\",\n\t\t\t\"{genres[2]}\"\n\t\t], \n"))
+                        + $"\t\t\"language\" : \"{language}\",\n" + $"\t\t\"company\" : \"{company}\"\n" + "\t}";
+
                     string newJson = "";
                     for (int j = 0; j < len - 1; j++)
                     {
