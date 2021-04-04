6499701a715a1e9ad149e87f61412ede564557fc
bituq <d.noorland9@hotmail.com>
2021-04-04 17:46:19 +0200

    Cleaned up and fixed bugs

--- ConsoleApp1/JsonHandler.cs
+++ ConsoleApp1/JsonHandler.cs
@@ -1,29 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using CinemaApplication;
 
 namespace JsonHandler
 {
     public static class JsonFile
     {
+        private static JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+        private static void OverwriteFile(string filePath, List<JsonElement> docList, JsonSerializerOptions options) =>
+            File.WriteAllText(filePath, JsonSerializer.Serialize(docList.ToArray(), options));
+        private static List<JsonElement> FileAsList(string filePath)
+        {
+            var doc = JsonDocument.Parse(File.ReadAllText(filePath));
+            return new List<JsonElement>(doc.RootElement.EnumerateArray());
+        }
+
         public static void AppendToFile(object value, string filePath)
         {
-            var options = new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = true,
-            };
+            var newItem = JsonDocument.Parse(JsonSerializer.Serialize(value, options));
+            var docList = FileAsList(filePath);
+            docList.Add(newItem.RootElement[0]);
+            OverwriteFile(filePath, docList, options);
+        }
 
-            string oldFile = File.ReadAllText(filePath);
-            JsonDocument doc = JsonDocument.Parse(oldFile);
-            var docList = new List<JsonElement>(doc.RootElement.EnumerateArray());
-            var newItem = JsonSerializer.Serialize(value, options);
-            var newItemInJson = JsonDocument.Parse(newItem);
-            docList.Add(newItemInJson.RootElement[0]);
-            File.WriteAllText(filePath, JsonSerializer.Serialize(docList.ToArray(), options));
+        public static void RemoveFromFile(string key, string value, string filePath)
+        {
+            var oldList = FileAsList(filePath);
+            var newList = new List<JsonElement>(oldList);
+            foreach (JsonElement obj in oldList)
+            {
+                if (obj.GetProperty(key).ToString() == value)
+                    newList.Remove(obj);
+            }
+            OverwriteFile(filePath, newList, options);
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,40 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using AppComponents;
 using JsonHandler;
 
 namespace CinemaApplication
 {
-    public static class Defaults
-    {
-        public static NavigationMenu DefaultNavMenu(Tab tab, string[] items, Tab[] tabs)
-        {
-            return Templates.EasyNavigationMenu(
-                    tab,
-                    new Anchor(0, 1),
-                    items,
-                    tabs,
-                    Templates.ColorPalettes.CasualMonochrome,
-                    "Navigeren"
-                );
-        }
-    }
-
     public class Program
     {
-        public static class Screens
-        {
-            public static Tab mainMenu = new Tab(true);
-            public static Tab movieScreen = new Tab();
-            public static Tab adminScreen = new Tab();
-            public static Tab editScreen = new Tab();
-        }
         public class Movie
         {
-            public Tab tab = new Tab();
             public string name { get; set; }
             public int duration { get; set; }
             public int releaseDate { get; set; }
@@ -44,109 +18,45 @@ namespace CinemaApplication
             public string company { get; set; }
             public string[] starring { get; set; }
             public string description { get; set; }
-
-            public Selectable saveMovieInfo() => new Builders.ListBuilder(
-                    this.tab,
-                    new Anchor(30, 1),
-                    new string[]
-                    {
-                        $"Naam: {name}",
-                        $"Lengte: {DateTime.UnixEpoch.AddSeconds(duration).ToString($"HH uur m")} minuten",
-                        $"Publicatie: {DateTime.UnixEpoch.AddSeconds(releaseDate).ToString("dd MMMM yyyy")}",
-                        $"Rating: {rating}/5",
-                        $"Taal: {language}",
-                        $"Bedrijf: {company}",
-                        $"Beschrijving: {description}"
-                    },
-                    DefaultColor: Templates.Colors.GrayBlack
-                )
-                .AsSelectable(Templates.Colors.WhiteBlack, "Informatie over deze film")
-                .Done();
-        }
-        public static void MovieScreen(Tab tab)
-        {
-
-            string movies = File.ReadAllText("Movies.json");
-            JsonDocument doc = JsonDocument.Parse(movies);
-            JsonElement root = doc.RootElement;
-
-            string[] moviesArray = new string[root.GetArrayLength()];
-            Tab[] tabs = new Tab[moviesArray.Length];
-
-            for (int i = 0; i < moviesArray.Length; i++)
-            {
-                var movie = JsonSerializer.Deserialize<Movie>(root[i].ToString());
-                var navMenuMovie = Defaults.DefaultNavMenu(
-                movie.tab,
-                new string[] { "Hoofdmenu", "Bekijk reservering", "Terug naar films" },
-                new Tab[] { Screens.mainMenu, tab, tab }
-                );
-                movie.saveMovieInfo();
-                moviesArray[i] =
-                    $"{root[i].GetProperty("name")}\t\t" +
-                    $"{root[i].GetProperty("rating")}/5\t" +
-                    $"{DateTime.UnixEpoch.AddSeconds(root[i].GetProperty("releaseDate").GetInt32()):dd MMMM yyyy}";
-                tabs[i] = movie.tab;
-            }
-            var navMenu = Defaults.DefaultNavMenu(
-                tab,
-                new string[] { "Hoofdmenu", "Bekijk reservering", "Admin paneel" },
-                new Tab[] { Screens.mainMenu, tab, Screens.adminScreen}
-                );
-            var list = Templates.MoviesList(
-                tab,
-                new Anchor(30, 1),
-                moviesArray,
-                tabs,
-                Templates.ColorPalettes.FadedMonochrome,
-                "NAAM\t\t\t\tRATING\tRELEASE"
-                );
-        }
-        public static void MainMenu(Tab tab)
-        {
-            var menu = Templates.EasyNavigationMenu(
-                    tab,
-                    new Anchor(3, 1),
-                    new string[] { "Bekijk films", "Bekijk reservering", "Admin paneel" },
-                    new Tab[] { Screens.movieScreen, tab, Screens.adminScreen },
-                    Templates.ColorPalettes.CasualMonochrome,
-                    "Project B Bioscoop Applicatie"
-                );
-            
-        }
-        public static void AdminPanel(Tab tab)
-        {
-            var navMenu = Defaults.DefaultNavMenu(
-                tab,
-                new string[] { "Hoofdmenu", "Bekijk films" },
-                new Tab[] { Screens.mainMenu, Screens.movieScreen }
-                );
         }
         public static void Main()
         {
-            while (true)
-            {
-                var movie = new Movie();
-                Console.WriteLine("Voeg hier een film toe");
-                Console.Write("Naam: ");
-                movie.name = Console.ReadLine();
-                Console.Write("Lengte (seconden): ");
-                movie.duration = Int32.Parse(Console.ReadLine());
-                Console.Write("Publicatiedatum (unix): ");
-                movie.releaseDate = Int32.Parse(Console.ReadLine());
-                Console.Write("Rating: ");
-                movie.rating = Int32.Parse(Console.ReadLine());
-                Console.Write("Taal: ");
-                movie.language = Console.ReadLine();
-                Console.Write("Bedrijf: ");
-                movie.company = Console.ReadLine();
-                Console.Write("Beschrijving: ");
-                movie.description = Console.ReadLine();
-                JsonHandler.JsonFile.AppendToFile(new List<Movie> { movie }, "Movies.json");
-                Console.WriteLine($"Successfully added \"{movie.name}\"");
-                Console.WriteLine("\nPress any key to continue");
-                Console.ReadKey();
-            }
+            var movie = new Movie();
+            movie.name = "Hacksaw Ridge";
+            movie.duration = 5000;
+            movie.releaseDate = 1478127600;
+            movie.rating = 5;
+            movie.language = "Engels";
+            movie.company = "Lionsgate";
+            movie.description = "Nothing to see here...";
+
+            JsonFile.AppendToFile(new List<Movie> { movie }, "Movies.json");
+            Console.WriteLine($"Successfully added \"{movie.name}\"");
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+
+            JsonFile.RemoveFromFile("name", "Hacksaw Ridge", "Movies.json");
+            Console.WriteLine($"Successfully removed \"{movie.name}\"");
+
+            /*Console.WriteLine("Voeg hier een film toe");
+            Console.Write("Naam: ");
+            movie.name = Console.ReadLine();
+            Console.Write("Lengte (seconden): ");
+            movie.duration = Int32.Parse(Console.ReadLine());
+            Console.Write("Publicatiedatum (unix): ");
+            movie.releaseDate = Int32.Parse(Console.ReadLine());
+            Console.Write("Rating: ");
+            movie.rating = Int32.Parse(Console.ReadLine());
+            Console.Write("Taal: ");
+            movie.language = Console.ReadLine();
+            Console.Write("Bedrijf: ");
+            movie.company = Console.ReadLine();
+            Console.Write("Beschrijving: ");
+            movie.description = Console.ReadLine();
+            JsonHandler.JsonFile.AppendToFile(new List<Movie> { movie }, "Movies.json");
+            Console.WriteLine($"Successfully added \"{movie.name}\"");
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();*/
         }
     }
 }
