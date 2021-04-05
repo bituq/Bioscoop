d7371b09a53bdc21d8a88c1569604d7d973df57e
WonderOfYou <75250387+WonderOfYou@users.noreply.github.com>
2021-04-05 14:41:09 +0200

    added list

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,22 +11,73 @@ namespace CinemaApplication
             var movies = File.ReadAllText("Movies.json");
 
             JsonDocument doc = JsonDocument.Parse(movies);
+            JsonElement root = doc.RootElement;
 
-            foreach (JsonElement movie in doc.RootElement.EnumerateArray())
+            var u1 = root[0];
+            var u2 = root[1];
+            var u3 = root[2];
+
+            Console.WriteLine("1 : Harry Potter and the Deathly Hallows: Part 1\n2 : Lord of the Rings: Fellowship of the Ring\n3 : 1917");
+            Console.Write("Maak uw Keuze: ");
+            string movieList = Console.ReadLine();
+            if (movieList.Contains("1") == true)
             {
-                
+                Console.WriteLine(u1.GetProperty("name"));
+                Console.WriteLine(u1.GetProperty("duration"));
+                Console.WriteLine(u1.GetProperty("language"));
+                Console.WriteLine(u1.GetProperty("company"));
+                Console.WriteLine("starring:");
+                foreach (JsonElement starring in u1.GetProperty("starring").EnumerateArray())
                 {
-                    Console.WriteLine(movie.GetProperty("name"));
-                    Console.WriteLine($"{movie.GetProperty("duration")}");
-                    foreach (JsonElement starring in movie.GetProperty("starring").EnumerateArray())
-                    {
-                        Console.WriteLine($"\t{starring}");
-                    }
-                    Console.WriteLine(movie.GetProperty("rating"));
-                    Console.WriteLine(movie.GetProperty("description"));
-                    Console.WriteLine("");
+                    Console.WriteLine(starring);
                 }
+                Console.WriteLine("genre(s):");
+                foreach (JsonElement starring in u1.GetProperty("genres").EnumerateArray())
+                {
+                    Console.WriteLine(starring);
+                }
+                Console.WriteLine(u1.GetProperty("releaseDate"));
+                Console.WriteLine(u1.GetProperty("rating"));
+            }
+            else if (movieList.Contains("2") == true)
+            {
+                Console.WriteLine(u2.GetProperty("name"));
+                Console.WriteLine(u2.GetProperty("duration"));
+                Console.WriteLine(u2.GetProperty("language"));
+                Console.WriteLine(u2.GetProperty("company"));
+                Console.WriteLine("starring:");
+                foreach (JsonElement starring in u2.GetProperty("starring").EnumerateArray())
+                {
+                    Console.WriteLine(starring);
+                }
+                Console.WriteLine("genre(s):");
+                foreach (JsonElement starring in u2.GetProperty("genres").EnumerateArray())
+                {
+                    Console.WriteLine(starring);
+                }
+                Console.WriteLine(u2.GetProperty("releaseDate"));
+                Console.WriteLine(u2.GetProperty("rating"));
+            }
+            else if (movieList.Contains("3") == true)
+            {
+                Console.WriteLine(u3.GetProperty("name"));
+                Console.WriteLine(u3.GetProperty("duration"));
+                Console.WriteLine(u3.GetProperty("language"));
+                Console.WriteLine(u3.GetProperty("company"));
+                Console.WriteLine("starring:");
+                foreach (JsonElement starring in u3.GetProperty("starring").EnumerateArray())
+                {
+                    Console.WriteLine(starring);
+                }
+                Console.WriteLine("genre(s):");
+                foreach (JsonElement starring in u3.GetProperty("genres").EnumerateArray())
+                {
+                    Console.WriteLine(starring);
+                }
+                Console.WriteLine(u3.GetProperty("releaseDate"));
+                Console.WriteLine(u3.GetProperty("rating"));
             }
         }
     }
+
 }
