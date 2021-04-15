66310babc46a917a071ee4bf36f86a45a66b48ba
Mohammed Tarkhany <0993790@hr.nl>
2021-04-15 16:59:36 +0200

    w

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
-using System.Text.Json;
 using System.IO;
+using System.Collections.Generic;
+using JsonHandler;
 
 namespace CinemaApplication
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public class halls
         {
-        
+            public string name { get; set; }
+            public int rows { get; set; }
+            public int columns { get; set; }
+            public int[][] seats { get; set; }
+        }
+        public static void Main()
+        {
+            var hall = new halls();
+            hall.name = "hall3";
+            hall.rows = 96;
+            hall.columns = 32;
+            hall.seats = new int[2][] {
+            new int[2]{3, 2},
+            new int[3]{4, 1, 4}
+            };
+
+            
+            
+
+            JsonFile.AppendToFile(new List<halls> { hall }, "halls.json");
+            Console.WriteLine($"Successfully added \"{hall.name}\"");
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+
+            //JsonFile.RemoveFromFile("name", "hall1", "halls.json");
+            //Console.WriteLine($"Successfully removed \"{hall.name}\"");
         }
     }
 }
