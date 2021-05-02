de4574675a21e2032bfc834e4a948ae04724d624
Dylan Noorland <1006575@hr.nl>
2021-05-02 12:57:28 +0200

    Added new folder

---
+++ ConsoleApp1/Program/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CinemaUI;
+using CinemaUI.Builder;
+
+namespace CinemaApplication.Program
+{
+    partial class Program
+    {
+        public static Window listOfMovies = new Window();
+    }
+}
--- ConsoleApp1/Program/Program.cs
+++ ConsoleApp1/Program/Program.cs
@@ -22,7 +22,7 @@ namespace CinemaApplication
             var menu = new TextListBuilder(mainMenu, 2, 5)
                 .Color(ConsoleColor.White)
                 .Selectable(new Color(ConsoleColor.Black, ConsoleColor.White), true, "Demonstratie", "Item 2", "Item 3")
-                .LinkWindows(listOfMovies)
+                .LinkWindows(listOfFilms)
                 .Result();
 
             var inputMenu = new TextListBuilder(mainMenu, 40, 5)
@@ -43,27 +43,12 @@ namespace CinemaApplication
             }
         }
 
-        static Window listOfMovies = new Window();
-        static void ListOfMovies()
-        {
-            var title = new TextBuilder(listOfMovies, 2, 2)
-                .Color(ConsoleColor.Yellow)
-                .Result("List of movies");
-
-            var menuItems = new string[] { "Hoofdmenu", "Geavanceerd zoeken" };
-            var menu = new TextListBuilder(listOfMovies, 2, 4)
-                .Color(ConsoleColor.Gray)
-                .Selectable(new Color(ConsoleColor.Black, ConsoleColor.White), true, menuItems)
-                .LinkWindows(mainMenu)
-                .Result();
-        }
-
         static void Main(string[] args)
         {
-            MainMenu();
-            ListOfMovies();
+            //MainMenu();
+            ListOfFilms();
 
-            InputHandler.WaitForInput();
+            //InputHandler.WaitForInput();
         }
     }
 }
