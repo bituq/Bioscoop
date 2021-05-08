666fba3e4675fbe999653ac2f6c05d7111e02f9d
Dylan Noorland <1006575@hr.nl>
2021-05-08 14:18:11 +0200

    added maakscherm

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -64,58 +64,5 @@ namespace CinemaApplication
             reserveringFile.Close();
             Console.ForegroundColor = ConsoleColor.White;
         }
-    
-        static void reserveringMaken() { // functie voor het maken van reserveringen.
-            string filePath = @"bin\Debug\netcoreapp3.1\Reserveringen.json"; // pakt de filepath.
-            StreamReader reserveringFile = new StreamReader(filePath); // leest het json bestand.
-            var reserveringen = reserveringFile.ReadToEnd(); // maakt een string van het jsonbestand.
-
-            JsonDocument doc = JsonDocument.Parse(reserveringen); // omzetten in JsonDocument.
-            JsonElement root = doc.RootElement; // root ofzo.
-
-            Random rd = new Random(); // random, zorgt ervoor dat string creatie mogelijk is.
-            string CreateString(int Length) { // maakt een string met gegeven lengte.
-                const string allowedChars = "0123456789"; // karakters waar de string uit mag bestaan.
-                char[] chars = new char[Length]; // lijst van karakters met gegeven lengte.
-                for (int i = 0; i < Length; i++) { // loop die random karakters toevoegt aan de character array.
-                    chars[i] = allowedChars[rd.Next(0, allowedChars.Length)]; // voegt random karakter toe aan array.
-                }
-                return new string(chars); // zet de lijst om in een string en stuurt het terug naar de functioncall.
-            }
-
-            string randomCode = CreateString(7); // maakt een random string uit nummers met lengte van 7.
-            
-            foreach (JsonElement reservering in root.EnumerateArray()) { // gaat door alle reserveringen heen.
-                if ((reservering.GetProperty("reserveringNummer").ToString()) == randomCode) { // kijkt of nieuwe code gelijk is aan een oudere.
-                    reserveringMaken(); // stuurt de data terug als de reserveringscode hetzelfde is zodat er een nieuwe gemaakt kan worden.
-                    return; // stopt de functie zodat die niet op wonderbaarlijke wijze doorgaat.
-                }
-            }
-             // nieuwe reserveringscode is niet hetzelfde als een voorgaande, dus kan doorgaan.
-            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds; // unixtijd.
-            string unixTime = unixTimestamp.ToString(); // tijd in unix formaat op dit huidige moment, omgezet in een string.
-            string datum = "11 april 2021 om 16:00"; // datum
-
-            string[] lijstReserveringen = reserveringen.Split('}'); // splitst de json string naar een list per reservering.
-            int len = lijstReserveringen.Length; // pakt de lengte van de array.
-            for (int i = 0; i < len-1; i++) { // loop om de curly brackets terug toe te voegen.
-                if (i == (len-2)) { // pakt de laatste reservering (niet degene die nu word gemaakt)
-                lijstReserveringen[i] = lijstReserveringen[i] + "},\n"; // voegt een curly bracket toe aan de laatste reservering samen met een komma.
-                }
-                else {
-                    lijstReserveringen[i] = lijstReserveringen[i] + "}"; // geeft een curly bracket aan de reserveringen terug
-                }
-            }
-            // string toevoegen gooit alle data die opgepakt is van de reservering in het reservering formaat van het json bestand.
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine($"U gaat naar de film . U heeft  stoel(en) met nummer(s)  onder uw naam staan. Uw reserveringscode is {randomCode}. Sla de code goed op!");
-            string newJson = ""; // definiert de string waar het bestand uit gaat bestaan.
-            for (int j = 0; j < len-1; j++) {
-                newJson = newJson + lijstReserveringen[j]; // voegt de oude reserveringen weer samen.
-            }
-            reserveringFile.Close(); // sluit het bestand zodat de data geschreven kan worden naar het bestand.
-            File.WriteAllText(filePath, newJson); // herschrijft het bestand met niewe reserveringsinformatie.
-            Console.ForegroundColor = ConsoleColor.White;
-        }
     }
 }
