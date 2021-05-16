2491ee8d74af57a599d6ab2da371e00a31f26bab
Dylan Noorland <1006575@hr.nl>
2021-05-16 15:22:04 +0200

    revert

--- ConsoleApp1/Program/Program.cs
+++ ConsoleApp1/Program/Program.cs
@@ -9,7 +9,7 @@ namespace CinemaApplication
 {
     partial class Program
     {
-        static Window mainMenu = new Window(true);
+        static Window mainMenu = new Window();
         static void MainMenu()
         {
             var title = new TextBuilder(mainMenu, 2, 2)
