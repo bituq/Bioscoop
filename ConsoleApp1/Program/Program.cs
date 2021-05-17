a25f31693b0ebc499a0d3922e8d568f606351757
WonderOfYou <75250387+WonderOfYou@users.noreply.github.com>
2021-05-17 11:47:03 +0200

    home screen

--- ConsoleApp1/Program/Program.cs
+++ ConsoleApp1/Program/Program.cs
@@ -9,19 +9,32 @@ namespace CinemaApplication
 {
     partial class Program
     {
-        static Window mainMenu = new Window(true);
-        static void MainMenu()
-        {
-            var title = new TextBuilder(mainMenu, 2, 2)
+        static Window homeScreen = new Window(true);
+        static Window mainMenu = new Window(false);
+        
+
+        static void HomeScreen()
+        { 
+          var home = new TextBuilder(homeScreen, 2, 2)
                 .Color(ConsoleColor.Cyan)
                 .Text("Bioscoop Applicatie")
                 .Result();
 
-            var subtitle = new TextBuilder(mainMenu, 2, 3)
-                .Color(ConsoleColor.DarkGray)
-                .Text("Project B")
-                .Result();
+          var subtitle = new TextBuilder(homeScreen, 2, 3)
+                 .Color(ConsoleColor.DarkGray)
+                 .Text("Project B")
+                 .Result();
 
+          var screen = new TextListBuilder(homeScreen, 2, 5)
+                  .Color(ConsoleColor.White)
+                  .UseNumbers()
+                  .SetItems("Bezoeker", "Admin")
+                  .Selectable(ConsoleColor.Black, ConsoleColor.White)
+                  .LinkWindows(mainMenu, mainMenu)
+                  .Result();
+        }
+        static void MainMenu()
+        {
             var menu = new TextListBuilder(mainMenu, 2, 5)
                 .Color(ConsoleColor.White)
                 .UseNumbers()
@@ -29,10 +42,18 @@ namespace CinemaApplication
                 .Selectable(ConsoleColor.Black, ConsoleColor.White)
                 .LinkWindows(listOfFilms, snacksWindow)
                 .Result();
+
+            var exit = new TextListBuilder(mainMenu, 1, 1)
+                .Color(ConsoleColor.White)
+                .SetItems("Go back")
+                .Selectable(ConsoleColor.Black, ConsoleColor.White)
+                .LinkWindows(homeScreen)
+                .Result();
         }
 
         static void Main(string[] args)
         {
+            HomeScreen();
             MainMenu();
             ListOfFilms();
             SnacksWindow();
