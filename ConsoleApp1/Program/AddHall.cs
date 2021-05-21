754152f95da735c61f550ecd174b8656f77bc67c
MT0993790 <0993790@hr.nl>
2021-05-21 18:51:11 +0200

    trying to make some error messages for AddHalls

--- ConsoleApp1/Program/AddHall.cs
+++ ConsoleApp1/Program/AddHall.cs
@@ -55,36 +55,67 @@ namespace CinemaApplication
                 .SetItems("")
                 .Result();
 
-            var snacksAndDrinks = File.ReadAllText("..\\..\\..\\Halls.json");
+            var halljson = File.ReadAllText("..\\..\\..\\Halls.json");
 
-            JsonDocument doc = JsonDocument.Parse(snacksAndDrinks);
+            JsonDocument doc = JsonDocument.Parse(halljson);
             JsonElement root = doc.RootElement;
 
-            /*goBack[0].OnClick = () =>
-            {
-                input.Replace(new TextListBuilder(addhallscreen, 2 + inputOptions.Items[2].Text.Length, 3)
-                .Color(ConsoleColor.Gray)
-                .SetItems("", "", "")
-                .AsInput(ConsoleColor.Gray, ConsoleColor.Black)
-                .Result();)
-            };*/
+            
 
             addButton[0].OnClick = () =>
             {
+                bool valid = true;
                 if (input[0].Value != "" && input[1].Value != "" && input[2].Value != "")
                 {
-                    var nSnack = new addHall();
-                    nSnack.id = Convert.ToInt32(input[0].Value);
-                    nSnack.rows = Convert.ToInt32(input[1].Value);
-                    nSnack.columns = Convert.ToInt32(input[2].Value);
+                    var objHall = new addHall();
+                    objHall.id = Convert.ToInt32(input[0].Value);
+                    objHall.rows = Convert.ToInt32(input[1].Value);
+                    objHall.columns = Convert.ToInt32(input[2].Value);
 
-                    JsonFile.AppendToFile(nSnack, "..\\..\\..\\Halls.json");
-                    message.Replace(
-                        new TextListBuilder(addhallscreen, 1, 10)
-                        .Color(ConsoleColor.Green)
-                        .SetItems("You have succesfully added a new snack to the list!")
-                        .Result());
+                    for (int i = 0; i < root.GetArrayLength(); i++)
+                    {
+                        if (Convert.ToInt32(root[i].GetProperty("id")) == objHall.id)
+                        {
+                            message.Replace(
+                            new TextListBuilder(addhallscreen, 1, 10)
+                            .Color(ConsoleColor.Green)
+                            .SetItems($"There already excist an hall with id {objHall.id}!")
+                            .Result());
+                            valid = false;
+                        }
+                    }
+                    if (valid == true)
+                    {
+                        JsonFile.AppendToFile(objHall, "..\\..\\..\\Halls.json");
+
+                        message.Replace(
+                            new TextListBuilder(addhallscreen, 1, 10)
+                            .Color(ConsoleColor.Green)
+                            .SetItems($"Hall {objHall.id} is succesfully added to the list!")
+                            .Result());
+                    }
                 }
+                else
+                {
+                    message.Replace(new TextListBuilder(addhallscreen, 1, 10)
+                    .Color(ConsoleColor.Green)
+                    .SetItems("Not all fields are filled in!")
+                    .Result());
+                }
+            };
+
+            goBack[0].OnClick = () =>
+            {
+                /*input.Replace(new TextListBuilder(addhallscreen, 2 + inputOptions.Items[2].Text.Length, 3)
+                .Color(ConsoleColor.Gray)
+                .SetItems("", "", "")
+                .AsInput(ConsoleColor.Gray, ConsoleColor.Black)
+                .Result());*/
+
+                message.Replace(new TextListBuilder(addhallscreen, 1, 10)
+                    .Color(ConsoleColor.Green)
+                    .SetItems("")
+                    .Result()); 
             };
         }
     }
