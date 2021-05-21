0d05a7602515ed7b571bcf83e930d840350a6176
WonderOfYou <75250387+WonderOfYou@users.noreply.github.com>
2021-05-21 12:56:56 +0200

    Succes message bug fixed

--- ConsoleApp1/Program/SnackChange.cs
+++ ConsoleApp1/Program/SnackChange.cs
@@ -14,9 +14,9 @@ namespace CinemaApplication
         public class SnacksAdd
         {
             public string name { get; set; }
-            public string price { get; set; }
+            public double price { get; set; }
             public string vegetarian { get; set; }
-            public string stock { get; set; }
+            public int stock { get; set; }
         }
 
         public static Window addSnack = new Window(false);
@@ -33,35 +33,42 @@ namespace CinemaApplication
                 .Result();
 
             var addButton = new TextListBuilder(addSnack, 1, 8)
-                   .Color(ConsoleColor.Green)
-                   .SetItems("Add")
-                   .Selectable(ConsoleColor.Black, ConsoleColor.White)
-                   .Result();
+                .Color(ConsoleColor.Green)
+                .SetItems("Add")
+                .Selectable(ConsoleColor.Black, ConsoleColor.White)
+                .Result();
             
-            var _ = new TextListBuilder(addSnack, 1, 10)
-                   .Color(ConsoleColor.White)
-                   .SetItems("Go back")
-                   .Selectable(ConsoleColor.Black, ConsoleColor.White)
-                   .LinkWindows(mainMenu)
-                   .Result();
+            var _ = new TextListBuilder(addSnack, 1, 12)
+                .Color(ConsoleColor.White)
+                .SetItems("Go back")
+                .Selectable(ConsoleColor.Black, ConsoleColor.White)
+                .LinkWindows(mainMenu)
+                .Result();
+            
+            var message = new TextListBuilder(addSnack)
+                .SetItems("")
+                .Result();
 
             var snacksAndDrinks = File.ReadAllText("..\\..\\..\\snacksAndDrinks.json");
 
             JsonDocument doc = JsonDocument.Parse(snacksAndDrinks);
             JsonElement root = doc.RootElement;
 
+
             addButton[0].OnClick = () =>
             {
                 var nSnack = new SnacksAdd();
                 nSnack.name = input[0].Value;
-                nSnack.price = input[1].Value;
+                nSnack.price = Convert.ToDouble(input[1].Value);
                 nSnack.vegetarian = input[2].Value;
-                nSnack.stock = input[3].Value;
+                nSnack.stock = Convert.ToInt32(input[3].Value);
                 JsonFile.AppendToFile(nSnack, "..\\..\\..\\snacksAndDrinks.json");
-                Console.WriteLine("You have succesfully added a new snack to the list!");
-
+                message.Replace(
+                    new TextListBuilder(addSnack, 1, 10)
+                    .Color(ConsoleColor.Green)
+                    .SetItems("You have succesfully added a new snack to the list!")
+                    .Result());
             };
         }
-
     }
 }
